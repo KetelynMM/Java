c518a4202811c6329ff87eddf2465024a1b32e5b
Ketelyn Medina <ketelynmedina5@gmail.com>
2023-03-14 12:52:02 -0300

    Projeto Hello World

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int teste = 12;
+		System.out.println("Hello World!");
+		System.out.println("\n\tOlá, turma 62. \n\t:)");
+		System.out.println("\nVáriavel teste:\n\t" + teste);
+	}
+
+}
