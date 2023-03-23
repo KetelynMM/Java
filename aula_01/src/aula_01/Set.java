af0caea584172fc6c41a1f11640f757fb3c1c867
Ketelyn Medina <ketelynmedina5@gmail.com>
2023-03-23 16:02:25 -0300

    Exercícios

--- aula_01/src/aula_01/Set.java
+++ aula_01/src/aula_01/Set.java
@@ -1,10 +1,30 @@
 package aula_01;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Set {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner leia = new Scanner(System.in);
+		HashSet<Integer> setInteiros = new HashSet<Integer>();
+		
+		System.out.println("Digite 10 números inteiros sem repeti-los.");
+		
+		for(int num = 0; num < 10; num ++) {
+			System.out.println("\n\tDigite um número: ");
+			setInteiros.add(leia.nextInt());
+		}
+		
+		Iterator<Integer> numsetInteiros = setInteiros.iterator();
+		
+		while(numsetInteiros.hasNext()) {
+			System.out.println(numsetInteiros.next());
+		}
+		
+		leia.close();
 	}
 
 }
