29e023cea8db5454c62f5f620163dca783fd94d1
Ketelyn Medina <ketelynmedina5@gmail.com>
2023-04-11 09:15:02 -0300

    aula

---
+++ aula_07/src/aula_07/Cliente.java
@@ -0,0 +1,75 @@
+package aula_07;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Cliente {
+	
+			// Atributos
+			private String nome;
+			private LocalDate data_nascimento;
+			private String telefone;
+			private String email;
+			private String endereco;
+			
+			// Método Construtor
+			public Cliente(String nome, LocalDate data_nascimento, String telefone, String email, String endereco) {
+				this.nome = nome;
+				this.data_nascimento = data_nascimento;
+				this.telefone = telefone;
+				this.email = email;
+				this.endereco = endereco;
+			}
+			
+			// Métodos GETs e SETs
+			public String getNome() {
+				return nome;
+			}
+
+			public void setNome(String nome) {
+				this.nome = nome;
+			}
+
+			public LocalDate getdata_nascimento() {
+				return data_nascimento;
+			}
+
+			public void setdata_nascimento(LocalDate data_nascimento) {
+				this.data_nascimento = data_nascimento;
+			}
+			
+			public String getTelefone() {
+				return telefone;
+			}
+
+			public void setTelefone(String telefone) {
+				this.telefone = telefone;
+			}
+
+			public String getEmail() {
+				return email;
+			}
+
+			public void setEmail(String email) {
+				this.email = email;
+			}
+
+			public String getEndereco() {
+				return endereco;
+			}
+
+			public void setEndereço(String endereco) {
+				this.endereco = endereco;
+			}
+			
+			// Métodos que eu escrevi
+			public void visualizar() {
+				System.out.println("Nome: " + this.getNome());
+				System.out.println("Data de Nascimento: " + this.getdata_nascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+				System.out.println("Telefone: " + this.getTelefone());
+				System.out.println("E-mail: " + this.getEmail());
+				System.out.println("Endereço: " + this.getEndereco());
+			}
+			
+		}
+
---
+++ aula_07/src/aula_07/testa_cliente.java
@@ -0,0 +1,21 @@
+package aula_07;
+
+import java.time.LocalDate;
+
+public class testa_cliente {
+
+	public static void main(String[] args) {
+		LocalDate data1 = LocalDate.of(1998, 03, 24);
+		LocalDate data2 = LocalDate.of(1972, 07, 05);
+
+		ClientePessoaFisica cliente1 = new ClientePessoaFisica("Lais Sales", data1, "(12) 99999-9999",
+				"email1@gmail.com", "Rua Tal, 123", "123.456.789-12");
+		ClientePessoaJuridica cliente2 = new ClientePessoaJuridica("Marisa Sales", data2, "(12) 99999-9999",
+				"email2@gmail.com", "Rua Tal, 123", "789.456.123-98");
+
+		cliente1.visualizar();
+		System.out.println("\n");
+		cliente2.visualizar();
+	}
+
+}
